979846947693dac395b448c04091f2d6ef3817b3
Sietse de Kaper <targeter@gmail.com>
2020-12-07 09:01:14 +0100

    Day 6

---
+++ src/main/scala/day06/Day06.scala
@@ -0,0 +1,25 @@
+package day06
+
+import shared._
+
+case object Day06 extends AocTools(day = 6) {
+
+  private def groups(input: String): Array[String] = input.split("\n\n")
+  private def groupsLines(input: String): Array[Array[Set[Char]]] = groups(input).map(_.split("\n").map(_.toSet))
+
+  def step1(input: String): Int = groupsLines(input).map(_.flatten.toSet.size).sum
+
+  def step2(input: String): Int =
+    groupsLines(input).map { groupLines =>
+      val allAnswers = groupLines.flatten.toSet
+      groupLines
+        .foldLeft(allAnswers)(_ intersect _)
+        .size
+    }.sum
+
+  def main(args: Array[String]): Unit = {
+    println("Step 1: " + step1(inputBlob))
+    println("Step 2: " + step2(inputBlob))
+  }
+
+}
---
+++ src/test/scala/day06/Day06Test.scala
@@ -0,0 +1,13 @@
+package day06
+
+import org.scalatest.OptionValues
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers
+
+class Day06Test extends AnyFunSuite with Matchers with OptionValues {
+
+  import Day06._
+
+  test("test") {}
+
+}
