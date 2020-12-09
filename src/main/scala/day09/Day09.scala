7f8bc3430bf9aa8cdd641725240e01aa7fe7c67a
Sietse de Kaper <targeter@gmail.com>
2020-12-09 07:40:26 +0100

    Day 9

---
+++ src/main/scala/day09/Day09.scala
@@ -0,0 +1,29 @@
+package day09
+
+import shared._
+
+case object Day09 extends AocTools(day = 9) {
+  def isValid(inlist: List[Long], target: Long): Boolean = inlist.combinations(2).exists(_.sum == target)
+
+  def firstInvalid(input: List[Long], preambleLength: Int = 25): Option[Long] =
+    input
+      .sliding(preambleLength + 1, 1)
+      .dropWhile(l => isValid(l.take(preambleLength), l.last))
+      .nextOption()
+      .map(_.last)
+
+  def findWeakness(input: List[Long], invalid: Long): Option[Long] =
+    (2 to input.length).view
+      .flatMap(input.sliding(_, 1))
+      .find(_.sum == invalid)
+      .map(result => result.min + result.max)
+
+  def main(args: Array[String]): Unit = {
+    val longs = inputLines.map(_.toLong)
+    val invalid = firstInvalid(longs)
+
+    println("Step 1: " + invalid)
+    println("Step 2: " + invalid.flatMap(findWeakness(longs, _)))
+  }
+
+}
---
+++ src/test/scala/day09/Day09Test.scala
@@ -0,0 +1,52 @@
+package day09
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.{Inspectors, OptionValues}
+
+class Day09Test extends AnyFunSuite with Matchers with OptionValues with Inspectors {
+
+  val example = """35
+                  |20
+                  |15
+                  |25
+                  |47
+                  |40
+                  |62
+                  |55
+                  |65
+                  |95
+                  |102
+                  |117
+                  |150
+                  |182
+                  |127
+                  |219
+                  |299
+                  |277
+                  |309
+                  |576""".stripMargin.split("\n").map(_.toLong).toList
+
+  import Day09._
+  test("isValid") {
+    isValid((1L to 25L).toList, 26L) must be(true)
+    isValid((1L to 25L).toList, 49L) must be(true)
+    isValid((1L to 25L).toList, 100L) must be(false)
+    isValid((1L to 25L).toList, 50L) must be(false)
+
+    val list = ((1L to 19L) ++ (21L to 25L) :+ 45L).toList
+    isValid(list, 26L) must be(true)
+    isValid(list, 64L) must be(true)
+    isValid(list, 65L) must be(false)
+    isValid(list, 66L) must be(true)
+  }
+
+  test("firstInvalid") {
+    firstInvalid(example, 5).value must be(127L)
+  }
+
+  test("findWeakness") {
+    findWeakness(example, 127).value must be(62)
+  }
+
+}
