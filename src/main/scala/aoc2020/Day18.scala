41e67f92c969ac237967736d7981e702eb73546a
Sietse de Kaper <targeter@gmail.com>
2020-12-23 07:34:00 +0100

    Day 18 Fix

--- src/main/scala/aoc2020/Day18.scala
+++ src/main/scala/aoc2020/Day18.scala
@@ -2,46 +2,42 @@ package aoc2020
 
 import shared._
 
-import scala.collection.Set
 import scala.util.parsing.combinator.JavaTokenParsers
 
 case object Day18 extends AocTools(day = 18) with JavaTokenParsers {
-//  val NumberPattern = """\d""".r
-//  val OperationPattern = """(\d+) ([*|+]) (\d+)""".r
-//
-//  sealed trait Operand {
-//    def resolve: Long
-//    def add(oper: Operand): Op
-//    def mul(oper: Operand): Op
-//  }
-//  case class Num(value: Long) extends Operand {
-//    def add(num: Operand) = Op(this, Add, num)
-//    def mul(num: Operand) = Op(this, Mul, num)
-//    override def resolve: Long = value.toInt
-//  }
-//
-//  sealed trait Operator {
-//    def apply(left: Long, right: Long): Long
-//  }
-//  object Add extends Operator {
-//    override def apply(left: Long, right: Long): Long = left + right
-//  }
-//  object Mul extends Operator {
-//    override def apply(left: Long, right: Long): Long = left * right
-//  }
-//
-//  case class Op(left: Operand, operator: Operator, right: Operand) extends Operand {
-//    def resolve: Long = operator(left.resolve, right.resolve)
-//
-//    def add(num: Operand) = Op(this, Add, num)
-//    def mul(num: Operand) = Op(this, Mul, num)
-//  }
+  sealed trait Operand {
+    def resolve: Long
+    def add(oper: Operand): Op
+    def mul(oper: Operand): Op
+  }
+  case class Num(value: Long) extends Operand {
+    def add(num: Operand) = Op(this, Add, num)
+    def mul(num: Operand) = Op(this, Mul, num)
+    override def resolve: Long = value.toInt
+  }
+
+  sealed trait Operator {
+    def apply(left: Long, right: Long): Long
+  }
+  object Add extends Operator {
+    override def apply(left: Long, right: Long): Long = left + right
+  }
+  object Mul extends Operator {
+    override def apply(left: Long, right: Long): Long = left * right
+  }
+
+  case class Op(left: Operand, operator: Operator, right: Operand) extends Operand {
+    def resolve: Long = operator(left.resolve, right.resolve)
+
+    def add(num: Operand) = Op(this, Add, num)
+    def mul(num: Operand) = Op(this, Mul, num)
+  }
 
   object Parser extends JavaTokenParsers {
-    def num = wholeNumber ^^ (_.toLong)
-    def operand = num | "(" ~> expr <~ ")"
+    def num = "\\d+".r ^^ (_.toLong)
+    def simple = num | "(" ~> expr <~ ")"
     def expr: Parser[Long] =
-      (operand ~ rep(("+" | "*") ~ operand)) ^^ {
+      (simple ~ rep(("+" | "*") ~ simple)) ^^ {
         case a ~ rest =>
           rest.foldLeft(a) {
             case (a, "+" ~ b) => a + b
@@ -49,36 +45,28 @@ case object Day18 extends AocTools(day = 18) with JavaTokenParsers {
           }
       }
 
-    def parse(input: String) = parseAll(expr, input)
+    def parse(input: String): ParseResult[Long] = parseAll(expr, input)
 
   }
 
   object Parser2 extends JavaTokenParsers {
-    def num = wholeNumber ^^ (_.toLong)
-    def operand = num | "(" ~> expr <~ ")"
-    def expr: Parser[Long] =
-      (operand ~ rep("+" ~ operand)) ^^ {
-        case a ~ rest =>
-          rest.foldLeft(a) {
-            case (a, "+" ~ b) => a + b
-          }
-      }
+    def simple: Parser[Operand] = "\\d+".r ^^ ((v: String) => Num(v.toLong)) | "(" ~> expr <~ ")"
 
-    def expr2 =
-      (expr ~ rep("*" ~ expr)) ^^ {
-        case a ~ rest =>
-          rest.foldLeft(a) {
-            case (a, "*" ~ b) => a * b
-          }
-      }
+    def factor: Parser[Operand] = {
+      val function: (Operand, Operand) => Op = Op.apply(_, Add, _)
+      chainl1(simple, "+" ^^^ function)
+    }
+    def expr: Parser2.Parser[Operand] = {
+      val function: (Operand, Operand) => Op = Op(_, Mul, _)
+      chainl1(factor, "*" ^^^ function)
+    }
 
-    def parse(input: String) = parseAll(expr2, input)
-
+    def parse(input: String): Parser2.ParseResult[Long] = parseAll(expr, input).map(_.resolve)
   }
 
   def main(args: Array[String]): Unit = {
     println("Step 1: " + inputLines.map(Parser.parseAll(Parser.expr, _).get).sum)
-    println("Step 2: " + inputLines.map(Parser2.parse(_).getOrElse(0)))
+    println("Step 2: " + inputLines.map(Parser2.parse(_).get).sum)
   }
 
 }
